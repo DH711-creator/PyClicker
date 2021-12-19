010e552ae21e164f48b41ba55bb9aaf0dd60b2de
eVCANN <evanholland1780@gmail.com>
2021-12-19 12:21:02 -0500

    Autoclicker now works

--- PyClicker.py
+++ PyClicker.py
@@ -1,18 +1,37 @@
-from pyautogui import typewrite
+import pyautogui
+import pynput
+import time
+import random
 
 
-try:    
-    import pyautogui
-    import time
-    import random
+try:
+    def runclicker():
+        debug = input("Run in debug mode? y/n ")
 
+        mincps = 0.0
+        maxcps = 0.0
 
-    mincps = int(input("Enter Minimum CPS: ")) / 1000
-    maxcps = int(input("Enter Maximum CPS: ")) / 1000
+        def askcps():
+            global mincps, maxcps, amount
+            mincps = (1000 / float(input("Enter minimum CPS: ")))*0.001
+            if debug == 'y':
+                print(mincps)
+            maxcps = (1000 / float(input("Enter maximum CPS: ")))*0.001
+            if debug == 'y':
+                print(maxcps)
+            #Converting CPS into MS delay
+            amount = int(input("How many times would you like to click? "))
 
-    for i in range(1,70):
-        time.sleep(random.randint(mincps, maxcps))
-        pyautogui.leftClick
-        i = i+1
+        askcps()
+
+        for i in range(1, amount):
+            delay = random.uniform(mincps, maxcps)
+            if debug == 'y':
+                print(delay)
+            time.sleep(delay)
+            pyautogui.click()
+            i = i+1
+    runclicker()
 except TypeError:
-    print('bad')
+    print("Please enter an integer or float value for CPS, and an integer value for amount.")
+    runclicker()

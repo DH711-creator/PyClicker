4fc61e0383caf24d17a4fcf947019ab427685aab
eVCANN <evanholland1780@gmail.com>
2021-12-18 20:55:40 -0500

    did a bit I guess but I'm getting errors with random

--- PyClicker.py
+++ PyClicker.py
@@ -1,1 +1,18 @@
-print("clicked 1000 cps or something")
+from pyautogui import typewrite
+
+
+try:    
+    import pyautogui
+    import time
+    import random
+
+
+    mincps = int(input("Enter Minimum CPS: ")) / 1000
+    maxcps = int(input("Enter Maximum CPS: ")) / 1000
+
+    for i in range(1,70):
+        time.sleep(random.randint(mincps, maxcps))
+        pyautogui.leftClick
+        i = i+1
+except TypeError:
+    print('bad')

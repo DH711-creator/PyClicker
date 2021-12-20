527a4ce89b6fbc95e1d64c2f60619372a95afd09
DH711-creator <83188352+DH711-creator@users.noreply.github.com>
2021-12-19 22:18:53 -0500

    Update PyClicker.py
    removed unnecessary function and global variables

--- Versions/PyClicker.py
+++ Versions/PyClicker.py
@@ -1,5 +1,4 @@
 import pyautogui
-import pynput
 import time
 import random
 
@@ -7,18 +6,12 @@ import random
 try:
     def runclicker():
 
-        mincps = 0.0
-        maxcps = 0.0
-
-        def askcps():
-            global mincps, maxcps, amount
-            mincps = (1000 / float(input("Enter minimum CPS: ")))*0.001
-            maxcps = (1000 / float(input("Enter maximum CPS: ")))*0.001
-            #Converting CPS into MS delay
-            amount = int(input("How many times would you like to click? "))
-
-        askcps()
-
+        
+        mincps = (1000 / float(input("Enter minimum CPS: ")))*0.001
+        maxcps = (1000 / float(input("Enter maximum CPS: ")))*0.001
+        #Converting CPS into MS delay
+        amount = int(input("How many times would you like to click? "))
+            
         for i in range(1, amount):
             delay = random.uniform(mincps, maxcps)
             time.sleep(delay)
@@ -26,5 +19,5 @@ try:
             i = i+1
     runclicker()
 except TypeError:
-    print("Please enter an integer or float value for CPS, and an integer value for amount.")
+    print("Please enter an integer or float value for CPS, and an integer value for amount.") 
     runclicker()
